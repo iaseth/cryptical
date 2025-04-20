cedef3090a2591f6e1ffbe9ad896fff8d6396048
Ankur Seth <iankurseth@gmail.com>
2025-04-20 13:00:29 +0530

    Create a basic jwt decoder

--- sv-app/src/routes/+page.svelte
+++ sv-app/src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<script lang="ts">
+	import { jwtDecode } from "jwt-decode";
+
+	let jwtToken = $state('');
+	let decodedToken = $state({});
+
+	function decodeJwtToken (token: string) {
+		try {
+			return jwtDecode(jwtToken);
+		} catch (error) {
+			return {};
+		}
+	}
+
+	function onclick () {
+		decodedToken = decodeJwtToken(jwtToken);
+	}
+
+	$inspect(decodedToken);
+</script>
+
+<section class="max-w-3xl mx-auto p-4 overflow-x-hidden">
+	<header class="space-y-4 mb-6">
+		<textarea class="textarea block w-full" bind:value={jwtToken} rows=10></textarea>
+		<button {onclick} class="btn btn-primary">Decode</button>
+	</header>
+
+	<footer class="px-4 py-4 bg-base-200">
+		<pre class="text-wrap break-words">{JSON.stringify(decodedToken, null, 4)}</pre>
+	</footer>
+</section>
