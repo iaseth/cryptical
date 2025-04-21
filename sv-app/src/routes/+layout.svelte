3aa02fa2e332321559db3b2cb5d101b264e08383
Ankur Seth <iankurseth@gmail.com>
2025-04-21 13:22:45 +0530

    Added Hero

--- sv-app/src/routes/+layout.svelte
+++ sv-app/src/routes/+layout.svelte
@@ -2,14 +2,18 @@
 	import '../app.css';
 
 	let { children } = $props();
+
+	const headerClass = "grow basis-0 btn py-8 rounded-t-none";
 </script>
 
 <svelte:head>
 	<title>Cryptical</title>
 </svelte:head>
 
-<header class="text-center">
-	<a class="btn btn-lg rounded-t-none" href="/">Cryptical</a>
+<header class="container px-4 text-center flex gap-x-4 justify-between">
+	<a class={headerClass} href="/">Home</a>
+	<a class={headerClass} href="https://github.com/iaseth/cryptical">GitHub</a>
+	<a class={headerClass} href="https://github.com/iaseth">Author</a>
 </header>
 
 <main class="min-h-screen">
--- sv-app/src/routes/+page.svelte
+++ sv-app/src/routes/+page.svelte
@@ -3,6 +3,16 @@
 	import AppLink from "../lib/components/AppLink.svelte";
 </script>
 
+<div class="hero bg-base-200 py-20">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">Cryptical</h1>
+			<p class="py-6">This is just a small collection of single page apps related to Cryptography and Web Scraping.</p>
+			<a class="btn btn-primary" href="https://github.com/iaseth/cryptical">GitHub</a>
+		</div>
+	</div>
+</div>
+
 {#each sections as section}
 <section class="container px-4 py-12 space-y-8">
 	<header class="prose text-center mx-auto">
