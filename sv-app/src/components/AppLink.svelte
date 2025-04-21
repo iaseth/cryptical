8bcf7ed7883c27241ad957a25ac4b34329ae11f4
Ankur Seth <iankurseth@gmail.com>
2025-04-21 10:25:13 +0530

    Moved apps data to apps.json

--- sv-app/src/components/AppLink.svelte
+++ sv-app/src/components/AppLink.svelte
@@ -8,15 +8,11 @@
 	let { href, title, description }: Props = $props();
 </script>
 
-<div class="card bg-base-200 shadow-sm">
+<a {href} class="card bg-base-200 shadow-sm">
 	<div class="card-body">
 		<h2 class="card-title">{title}</h2>
 		{#if description}
 			<p>{description}</p>
 		{/if}
-
-		<div class="justify-end card-actions">
-			<a {href} class="btn btn-primary">Open</a>
-		</div>
 	</div>
-</div>
+</a>
--- sv-app/src/routes/+page.svelte
+++ sv-app/src/routes/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
-	import CardLink from "../components/CardLink.svelte";
+	import sections from "$lib/data/apps.json";
+	import AppLink from "../components/AppLink.svelte";
 </script>
 
-<section class="container px-4 py-12 grid gap-4 sm:grid-cols-2 md:grid-cols-3">
-	<CardLink href="/jwt" title="JWT"
-		description="Decode JWT tokens and more." />
-	<CardLink href="/curl" title="cURL"
-		description="Convert cURL commands to Code." />
+{#each sections as section}
+<section class="container px-4 py-12 space-y-8">
+	<header class="prose text-center mx-auto">
+		<h3>{section.title}</h3>
+	</header>
+
+	<section class="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
+		{#each section.apps as app}
+			<AppLink {...app} />
+		{/each}
+	</section>
 </section>
+{/each}
