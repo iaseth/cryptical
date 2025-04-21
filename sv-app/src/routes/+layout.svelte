8bb82d2b5602b85c5c1a58bd0848ef3fbc8a9199
Ankur Seth <iankurseth@gmail.com>
2025-04-21 09:54:45 +0530

    Added header and footer

--- sv-app/src/routes/+layout.svelte
+++ sv-app/src/routes/+layout.svelte
@@ -8,4 +8,18 @@
 	<title>Cryptical</title>
 </svelte:head>
 
-{@render children()}
+<header class="text-center">
+	<a class="btn btn-lg rounded-t-none" href="/">Cryptical</a>
+</header>
+
+<main class="min-h-screen">
+	{@render children()}
+</main>
+
+<footer class="prose text-center mx-auto px-4 py-20">
+	<h4>
+		<a class="link" href="https://github.com/iaseth">Profile</a>
+		<span> / </span>
+		<a class="link" href="https://github.com/iaseth/cryptical">Repo</a>
+	</h4>
+</footer>
