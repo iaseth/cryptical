22451471406260af58aea66b00e732c6153165d0
Ankur Seth <iankurseth@gmail.com>
2025-04-22 06:57:12 +0530

    Moved ts code into separate file

--- sv-app/src/routes/apps/hashing/+page.svelte
+++ sv-app/src/routes/apps/hashing/+page.svelte
@@ -2,66 +2,13 @@
 <script lang="ts">
 	import CodeBlock from "$lib/components/CodeBlock.svelte";
 	import TextArea from "$lib/components/TextArea.svelte";
-	import * as hashwasm from "hash-wasm";
-
-	type HashDefinition = {
-		group: string;
-		algo: string;
-		fn: (text: string) => Promise<string>;
-	};
-
-	type HashDS = {
-		group: string;
-		algo: string;
-		hash: string;
-	};
+	import { getHashes, GROUPS, type HashDS } from "./utils";
 
 	let inputText = $state('Hello, World!');
 	let hashes: HashDS[] = $state([]);
 
-	export const GROUPS = [
-		'SHA-1/2',
-		// 'SHA-3',
-		'BLAKE',
-		'Legacy',
-		'Non-Crypto'
-	];
-
-	export const hashAlgorithms: HashDefinition[] = [
-		// SHA-1/2
-		{ group: 'SHA-1/2', algo: 'SHA-1', fn: hashwasm.sha1 },
-		{ group: 'SHA-1/2', algo: 'SHA-256', fn: hashwasm.sha256 },
-		{ group: 'SHA-1/2', algo: 'SHA-384', fn: hashwasm.sha384 },
-		{ group: 'SHA-1/2', algo: 'SHA-512', fn: hashwasm.sha512 },
-
-		// BLAKE
-		{ group: 'BLAKE', algo: 'BLAKE2b', fn: (text) => hashwasm.blake2b(text, 64) },
-		{ group: 'BLAKE', algo: 'BLAKE2s', fn: (text) => hashwasm.blake2s(text, 32) },
-		{ group: 'BLAKE', algo: 'BLAKE3', fn: hashwasm.blake3 },
-
-		// Legacy
-		{ group: 'Legacy', algo: 'MD4', fn: hashwasm.md4 },
-		{ group: 'Legacy', algo: 'MD5', fn: hashwasm.md5 },
-		{ group: 'Legacy', algo: 'RIPEMD-160', fn: hashwasm.ripemd160 },
-		{ group: 'Legacy', algo: 'Whirlpool', fn: hashwasm.whirlpool },
-
-		// Non-Crypto
-		{ group: 'Non-Crypto', algo: 'CRC32', fn: hashwasm.crc32 },
-		{ group: 'Non-Crypto', algo: 'CRC64', fn: hashwasm.crc64 },
-		{ group: 'Non-Crypto', algo: 'xxHash32', fn: hashwasm.xxhash32 },
-		{ group: 'Non-Crypto', algo: 'xxHash64', fn: hashwasm.xxhash64 }
-	];
-
 	async function onclick () {
-		const text = inputText;
-		hashes = [];
-		for (const algo of hashAlgorithms) {
-			hashes.push({
-				group: algo.group,
-				algo: algo.algo,
-				hash: await algo.fn(text)
-			});
-		}
+		hashes = await getHashes(inputText);
 	}
 </script>
 
---
+++ sv-app/src/routes/apps/hashing/utils.ts
@@ -0,0 +1,60 @@
+import * as hashwasm from "hash-wasm";
+
+
+
+type HashDefinition = {
+	group: string;
+	algo: string;
+	fn: (text: string) => Promise<string>;
+};
+
+export type HashDS = {
+	group: string;
+	algo: string;
+	hash: string;
+};
+
+export const GROUPS = [
+	'SHA-1/2',
+	// 'SHA-3',
+	'BLAKE',
+	'Legacy',
+	'Non-Crypto'
+];
+
+export const hashAlgorithms: HashDefinition[] = [
+	// SHA-1/2
+	{ group: 'SHA-1/2', algo: 'SHA-1', fn: hashwasm.sha1 },
+	{ group: 'SHA-1/2', algo: 'SHA-256', fn: hashwasm.sha256 },
+	{ group: 'SHA-1/2', algo: 'SHA-384', fn: hashwasm.sha384 },
+	{ group: 'SHA-1/2', algo: 'SHA-512', fn: hashwasm.sha512 },
+
+	// BLAKE
+	{ group: 'BLAKE', algo: 'BLAKE2b', fn: (text) => hashwasm.blake2b(text, 64) },
+	{ group: 'BLAKE', algo: 'BLAKE2s', fn: (text) => hashwasm.blake2s(text, 32) },
+	{ group: 'BLAKE', algo: 'BLAKE3', fn: hashwasm.blake3 },
+
+	// Legacy
+	{ group: 'Legacy', algo: 'MD4', fn: hashwasm.md4 },
+	{ group: 'Legacy', algo: 'MD5', fn: hashwasm.md5 },
+	{ group: 'Legacy', algo: 'RIPEMD-160', fn: hashwasm.ripemd160 },
+	{ group: 'Legacy', algo: 'Whirlpool', fn: hashwasm.whirlpool },
+
+	// Non-Crypto
+	{ group: 'Non-Crypto', algo: 'CRC32', fn: hashwasm.crc32 },
+	{ group: 'Non-Crypto', algo: 'CRC64', fn: hashwasm.crc64 },
+	{ group: 'Non-Crypto', algo: 'xxHash32', fn: hashwasm.xxhash32 },
+	{ group: 'Non-Crypto', algo: 'xxHash64', fn: hashwasm.xxhash64 }
+];
+
+export async function getHashes(text: string) {
+	const hashes: HashDS[] = [];
+	for (const algo of hashAlgorithms) {
+		hashes.push({
+			group: algo.group,
+			algo: algo.algo,
+			hash: await algo.fn(text)
+		});
+	}
+	return hashes;
+}
