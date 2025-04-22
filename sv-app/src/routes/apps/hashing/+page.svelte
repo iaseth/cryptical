4a4363d0a584b94a1f884472aa0a00865762a325
Ankur Seth <iankurseth@gmail.com>
2025-04-22 06:46:24 +0530

    Now showing hashes grouped together

--- sv-app/src/routes/apps/hashing/+page.svelte
+++ sv-app/src/routes/apps/hashing/+page.svelte
@@ -4,22 +4,64 @@
 	import TextArea from "$lib/components/TextArea.svelte";
 	import * as hashwasm from "hash-wasm";
 
-	interface HashDS {
-		algo: string,
-		hash: string
-	}
+	type HashDefinition = {
+		group: string;
+		algo: string;
+		fn: (text: string) => Promise<string>;
+	};
+
+	type HashDS = {
+		group: string;
+		algo: string;
+		hash: string;
+	};
 
 	let inputText = $state('Hello, World!');
 	let hashes: HashDS[] = $state([]);
 
+	export const GROUPS = [
+		'SHA-1/2',
+		// 'SHA-3',
+		'BLAKE',
+		'Legacy',
+		'Non-Crypto'
+	];
+
+	export const hashAlgorithms: HashDefinition[] = [
+		// SHA-1/2
+		{ group: 'SHA-1/2', algo: 'SHA-1', fn: hashwasm.sha1 },
+		{ group: 'SHA-1/2', algo: 'SHA-256', fn: hashwasm.sha256 },
+		{ group: 'SHA-1/2', algo: 'SHA-384', fn: hashwasm.sha384 },
+		{ group: 'SHA-1/2', algo: 'SHA-512', fn: hashwasm.sha512 },
+
+		// BLAKE
+		{ group: 'BLAKE', algo: 'BLAKE2b', fn: (text) => hashwasm.blake2b(text, 64) },
+		{ group: 'BLAKE', algo: 'BLAKE2s', fn: (text) => hashwasm.blake2s(text, 32) },
+		{ group: 'BLAKE', algo: 'BLAKE3', fn: hashwasm.blake3 },
+
+		// Legacy
+		{ group: 'Legacy', algo: 'MD4', fn: hashwasm.md4 },
+		{ group: 'Legacy', algo: 'MD5', fn: hashwasm.md5 },
+		{ group: 'Legacy', algo: 'RIPEMD-160', fn: hashwasm.ripemd160 },
+		{ group: 'Legacy', algo: 'Whirlpool', fn: hashwasm.whirlpool },
+
+		// Non-Crypto
+		{ group: 'Non-Crypto', algo: 'CRC32', fn: hashwasm.crc32 },
+		{ group: 'Non-Crypto', algo: 'CRC64', fn: hashwasm.crc64 },
+		{ group: 'Non-Crypto', algo: 'xxHash32', fn: hashwasm.xxhash32 },
+		{ group: 'Non-Crypto', algo: 'xxHash64', fn: hashwasm.xxhash64 }
+	];
+
 	async function onclick () {
 		const text = inputText;
-		hashes = [
-			{ algo: "SHA-1", hash: await hashwasm.sha1(text) },
-			{ algo: "SHA-256", hash: await hashwasm.sha256(text) },
-			{ algo: "SHA-384", hash: await hashwasm.sha384(text) },
-			{ algo: "SHA-512", hash: await hashwasm.sha512(text) },
-		];
+		hashes = [];
+		for (const algo of hashAlgorithms) {
+			hashes.push({
+				group: algo.group,
+				algo: algo.algo,
+				hash: await algo.fn(text)
+			});
+		}
 	}
 </script>
 
@@ -33,9 +75,19 @@
 		rows={5} title="Enter Text" />
 
 	{#if hashes.length > 0}
-		<section class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-			{#each hashes as hash}
-				<CodeBlock code={hash.hash} title={hash.algo} />
+		<section class="space-y-12">
+			{#each GROUPS as group}
+				<section>
+					<header class="prose mx-auto py-8 text-center">
+						<h2>{group}</h2>
+					</header>
+
+					<section class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+						{#each hashes.filter(h => h.group === group) as hash}
+							<CodeBlock code={hash.hash} title={hash.algo} />
+						{/each}
+					</section>
+				</section>
 			{/each}
 		</section>
 	{/if}
