77a70b1d6b27ba4f2384ae247c570d0931dd5bfc
Ankur Seth <iankurseth@gmail.com>
2025-04-22 10:27:41 +0530

    base64-decoder now works live

--- sv-app/src/routes/apps/base64-decoder/+page.svelte
+++ sv-app/src/routes/apps/base64-decoder/+page.svelte
@@ -4,11 +4,7 @@
 	import TextArea from "$lib/components/TextArea.svelte";
 
 	let inputText = $state('SGVsbG8sIFdvcmxkIQ==');
-	let base64decoded: string = $state("");
-
-	function onclick () {
-		base64decoded = atob(inputText);
-	}
+	let base64decoded: string = $derived(atob(inputText));
 </script>
 
 <svelte:head>
@@ -17,13 +13,12 @@
 
 <section class="container p-4 space-y-4">
 	<TextArea bind:value={inputText} key="base64-decoder-input"
-		submitText="Decode" onSubmit={onclick}
 		rows={5} title="Enter Base64" />
 
 	{#if base64decoded}
 		<section class="grid md:grid-cols-2 gap-4">
 			<CodeBlock code={inputText} title="Input" />
-			<CodeBlock code={base64decoded} title="Base64" />
+			<CodeBlock code={base64decoded} title="Decoded" />
 		</section>
 	{/if}
 </section>
